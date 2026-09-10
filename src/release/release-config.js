221a519ed8171142cc225c960b2686151088dbc4
AadiJ1in <ajtexas2022@gmail.com>
2026-09-10 00:20:50 -0400

    Define RC1 release and schema contract

---
+++ src/release/release-config.js
@@ -0,0 +1,8 @@
+export const APP_RELEASE = "axion-rc1";
+export const EXPECTED_SCHEMA_VERSION = "202609100004";
+export const SCHEMA_COMPATIBILITY_VERSION = 1;
+
+export const MOVEMENT_ENGINE_VERSION = "rc1-engine-v1";
+export const PROFILE_SCHEMA_VERSION = "rc1-profile-v1";
+export const REP_DETECTOR_VERSION = "rc1-rep-detector-v1";
+export const CALIBRATION_VERSION = "rc1-calibration-v1";
