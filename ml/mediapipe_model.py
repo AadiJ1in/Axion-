3189f80c888df3d37cfe708bb35c2579d16a0e0a
AadiJ1in <ajtexas2022@gmail.com>
2026-09-17 21:33:01 -0400

    Pin offline MediaPipe model identity

---
+++ ml/mediapipe_model.py
@@ -0,0 +1,8 @@
+"""Pinned MediaPipe pose model used by Axion's offline research pipeline."""
+
+DEFAULT_MEDIAPIPE_MODEL_ID = "pose-landmarker-lite-float16-v1"
+DEFAULT_MEDIAPIPE_MODEL_URL = (
+    "https://storage.googleapis.com/mediapipe-models/pose_landmarker/"
+    "pose_landmarker_lite/float16/1/pose_landmarker_lite.task"
+)
+DEFAULT_MEDIAPIPE_MODEL_SHA256 = "59929e1d1ee95287735ddd833b19cf4ac46d29bc7afddbbf6753c459690d574a"
